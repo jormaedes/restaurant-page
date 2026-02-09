74f96a26297efe96bf40ef4ad416f62f976390d2
Jormaedes Luís <jormahed@gmail.com>
2026-02-09 12:42:37 +0100

    botao que leva para a home ja foi configurado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
+import { createHome } from './js/home';
+import { createFooter } from './js/create-footer';
 
-console.log('Hello, welcome to the restaurant page!');
+const content = document.getElementById('content');
+const homeBtn = document.getElementById('home-btn');
+const menuBtn = document.getElementById('menu-btn');
+const aboutBtn = document.getElementById('about-btn');
+
+homeBtn.addEventListener('click', () => {
+	while (content.firstChild) {
+		content.removeChild(content.firstChild);
+	}
+	content.textContent = '';
+	content.appendChild(createHome());
+	content.appendChild(createFooter());
+});
