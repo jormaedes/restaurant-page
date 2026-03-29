02b4063705e0fcb1e53c5994defdfe42d1074290
Jormaedes Luis <jormahed@gmail.com>
2026-03-29 09:57:53 +0100

    feat: add models create class Dish

---
+++ src/js/models/dish.js
@@ -0,0 +1,10 @@
+export class Dish
+{
+	constructor(name, desc, price, categ, imgSrc) {
+		this.name = name;
+		this.desc = desc;
+		this.price = price;
+		this.category = categ;
+		this.imgSrc = imgSrc;
+	}
+}
