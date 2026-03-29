e541e1107d9ebf9665355831884cda4f3e9d79ba
Jormaedes Luis <jormahed@gmail.com>
2026-03-29 10:38:26 +0100

    fix: solve bug in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { Generator } from "webpack";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
 	mode: 'development',
 	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
-		path: path.resolve(import.meta.dirname, 'dist'),
+		path: path.resolve(__dirname, 'dist'),
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
