e80cbf73b3b8c24daaa8387f7eb0f5ef8bcb677a
Jormaedes Luis <jormahed@gmail.com>
2026-03-30 08:58:24 +0100

    feat: add new elements in footer

--- src/js/create-footer.js
+++ src/js/create-footer.js
@@ -1,8 +1,38 @@
 export function createFooter() {
 	const footer = document.createElement('footer');
 	footer.innerHTML = `
-		<p>Built by <a href="https://github.com/jormaedes" target="_blank">Jormaedes</a> as part of <a href="https://www.theodinproject.com/" target="_blank">The Odin Project</a></p>
-		<p>&copy; 2026 Restaurant page</p>
+		<div class="more-info">
+			<div class="logo">
+				Maison
+			</div>
+
+			<div class="extra-info">
+				<p class="footer-heading">Information</p>
+				<p>(+1) 99348 3989824 834</p>
+				<p>restarant@gmail.com</p>
+				<p>123 Skyline Avenue, New York 10001</p>
+			</div>
+
+			<div class="extra-info">
+				<p class="footer-heading">Opening hours</p>
+				<p>Monday to Thursday: 9:00 - 21:00</p>
+				<p>Friday: 8:00 - 21:00</p>
+				<p>Saturday: 9:30 - 15:00</p>
+			</div>
+
+			<div class="extra-info">
+				<p class="footer-heading">Social</p>
+				<div class="logo-social-media-container">
+					<a class="social-footer" href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a class="social-footer" href="#"><i class="ri-instagram-fill"></i></a>
+					<a class="social-footer" href="#"><i id="twitter-x" class="ri-twitter-x-fill"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="info-developer">
+			<p>Built by <a href="https://github.com/jormaedes" target="_blank">Jormaedes</a> as part of <a href="https://www.theodinproject.com/" target="_blank">The Odin Project</a></p>
+			<p>&copy; 2026 Restaurant page</p>
+		</div>
 	`;
 	return footer;
 }
